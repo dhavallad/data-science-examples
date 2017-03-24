531935ad77538fc8f7b8d45f88cb8db751682ff9
dhavallad <dhavallad92@gmail.com>
2017-03-23 23:37:12 -0400

    new fucntion added to find tweets based on keyword in query.

--- CollectTweets/rest_api_example.py
+++ CollectTweets/rest_api_example.py
@@ -7,10 +7,10 @@ from pattern.en import positive
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-myApi=twitter.Api(consumer_key ='Enter consumer key',
-                  consumer_secret='Enter consumer secret',
-                  access_token_key='Enter access token key',
-                  access_token_secret='Enter access token secret')
+myApi=twitter.Api(consumer_key ='ENTER YOUR COSUMER KEY',
+                  consumer_secret='ENTER YOUR COSUMER SECRET',
+                  access_token_key='ENTER YOUR ACCESS TOKEN KEY',
+                  access_token_secret='ENTER YOUR ACCESS TOKEN SECRET')
 
 def randomsample():
     geo = ('32.7864', '-79.9408', '200mi')
@@ -29,10 +29,27 @@ def randomsample():
         if tweets:
             MAX_ID = tweets[-1]['id']
          
+def mainquery():
+    query = '(Trump OR Cruz OR President OR news OR donald OR victory OR atlanta OR wins OR wons OR supporter OR america OR winner)'  
+    geo = ('40.7127', '-74.0059', '600mi') # New York City
+    MAX_ID = None
+    output_text = open('MainData.txt', 'w')
+    # output_ID = open('MainData_ID.txt', 'w')
+    for it in range(5): # Retrieve up to 200 tweets
+        tweets = [json.loads(str(raw_tweet)) for raw_tweet \
+                  in myApi.GetSearch(query, geo, count = 100, max_id = MAX_ID, result_type='recent')]
+        for raw_t in tweets:
+            output_text.write(str(raw_t['id'])+"-"+str(raw_t['text'])+"\n")
+            # output_ID.write(str(raw_t['text'])+"\n")
+        if tweets:
+            MAX_ID = tweets[-1]['id']
+            # print MAX_ID, len(tweets)
+
 def main():
-    
-    randomsample()
-    
+   
+    # randomsample()
+    # mainquery()
+   
 if __name__ == '__main__':
     main()
     
